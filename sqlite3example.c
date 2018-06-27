a92a13dbcda89bdea9b130f5ad85fecdfea3e786
cyberspouk <cyberspouk@gmail.com>
2018-06-27 16:19:40 +0300

    up

--- sqlite3example.c
+++ sqlite3example.c
@@ -4,7 +4,29 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+int opendbs(char *dbsname, sqlite3 *handler);
+
 int main() {
+    char *dbsname = "simple.sqlite";
+    sqlite3 *db;
+    sqlite3_stmt *res;
+
+    int result = opendbs(dbsname, db);
+    if (!result) {
+        fprintf(stderr, "error open/create database `%s`", dbsname);
+        sqlite3_close(db);
+        return 1;
+    }
+    
+
     printf("Sqlite3 current version %s\n", sqlite3_libversion());
+    return 0;
+}
+
+int opendbs(char *dbsname, sqlite3 *handler) {
+    int rc = sqlite3_open(dbsname, &handler);
+    if (rc != SQLITE_OK) {
+        return -1;
+    }
     return 0;
 }
